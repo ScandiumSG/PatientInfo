264dc85dd8657e3e53878679be4f7a87eeb67cde
Stian Gaustad <stian.k.gaustad@gmail.com>
2024-12-07 17:12:25 +0100

    Data - Added configuration data for DataContext

--- Backend/Data/DataContext.cs
+++ Backend/Data/DataContext.cs
@@ -5,6 +5,20 @@ namespace Backend.Data
 {
     public class DataContext: DbContext
     {
-        public required DbSet<Patient> Patients { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Patient> Patients { get; set; }
     }
 }
