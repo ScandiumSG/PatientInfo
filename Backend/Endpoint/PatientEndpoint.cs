53790ecbe467a244b99304a36901f2742c8b72ee
Stian Gaustad <stian.k.gaustad@gmail.com>
2024-12-07 15:51:23 +0100

    Endpoint - Scaffolded endpoint class

---
+++ Backend/Endpoint/PatientEndpoint.cs
@@ -0,0 +1,43 @@
+﻿using Backend.Models;
+using Backend.Repository;
+
+namespace Backend.Endpoint
+{
+    public static class PatientEndpoint
+    {
+        public static void PatientEndpointConfiguration(this WebApplication app) {
+            var endpointGroup = app.MapGroup("/patient/");
+
+            endpointGroup.MapGet("/", GetAll);
+            endpointGroup.MapGet("/{id}", GetSpecific);
+            endpointGroup.MapPost("/", CreatePatient);
+            endpointGroup.MapPut("/", UpdatePatient);
+            endpointGroup.MapDelete("/{id}", DeletePatient);
+        }
+
+        public static IResult GetAll() 
+        {
+            return TypedResults.Ok();
+        }
+
+        public static IResult GetSpecific(int id) 
+        { 
+            return TypedResults.Ok(); 
+        }
+
+        public static IResult CreatePatient(IRepository<PatientModel> repo) 
+        {
+            return TypedResults.Ok();
+        }
+
+        public static IResult UpdatePatient(IRepository<PatientModel> repo) 
+        {
+            return TypedResults.Ok();
+        }
+
+        public static IResult DeletePatient(IRepository<PatientModel> repo) 
+        {
+            return TypedResults.Ok();
+        }
+    }
+}
