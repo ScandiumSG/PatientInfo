39e2fe45da3de2df757e35f489c3ea52fb98e881
Stian Gaustad <stian.k.gaustad@gmail.com>
2024-12-07 16:01:07 +0100

    Patient - Added age calculation method

--- Backend/Models/Patient.cs
+++ Backend/Models/Patient.cs
@@ -9,5 +9,23 @@
         public DateTime DateOfBirth { get; set; }
 
         public List<string> Conditions { get; set; } = new List<string>();
+
+        public int CalculateAge() 
+        {
+            DateTime currentTime = DateTime.Now;
+
+            int PatientAge = 0;
+
+            if (currentTime.Month >= DateOfBirth.Month && currentTime.Day >= DateOfBirth.Day) 
+            {
+                PatientAge = currentTime.Year - DateOfBirth.Year;
+            } 
+            else 
+            {
+                PatientAge = currentTime.Year - DateOfBirth.Year - 1;
+            }
+
+            return PatientAge;
+        }
     }
 }
