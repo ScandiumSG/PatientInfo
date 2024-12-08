5c16a38e499d480c85941b549af8a32aeca0dc95
Stian Gaustad <stian.k.gaustad@gmail.com>
2024-12-08 20:47:47 +0100

    Frontend - Updated editing functionality for patient conditions

--- Frontend/src/components/ListView/ListNode/ListNode.jsx
+++ Frontend/src/components/ListView/ListNode/ListNode.jsx
@@ -3,9 +3,13 @@ import styles from "./ListNode.module.css";
 import { GetPatientEndpoint } from "../../../util/ConnectionUtil";
 
 const ListNode = ({ refetch, data }) => {
+    const [newCondition, setNewCondition] = useState("");
+    const [editingConditions, setEditingConditions] = useState(false);
+    const [localConditions, setLocalConditions] = useState(data.conditions);
+
     useEffect(() => {
-        console.log(data);
-    }, [data]);
+        setLocalConditions(data.conditions);
+    }, [data.conditions]);
 
     const handleDelete = async () => {
         const opt = {
@@ -24,6 +28,61 @@ const ListNode = ({ refetch, data }) => {
             });
     };
 
+    const handleAddCondition = async () => {
+        if (newCondition.trim()) {
+            const updatedConditions = [...localConditions, newCondition];
+            await updateBackendData(updatedConditions);
+        }
+    };
+
+    /**
+     * Remove a string from the array of strings in localConditions
+     * @param {The index of the string in the localCondition array to remove} index
+     */
+    const handleRemoveCondition = async (index) => {
+        const updatedConditions = localConditions.filter((_, i) => i !== index);
+        setLocalConditions(updatedConditions);
+        await updateBackendData(updatedConditions);
+    };
+
+    /**
+     * Change the value of a string at the targeted index of localConditions
+     * @param {ChangeEvent} e Change event
+     * @param {number} index Index in the localConditions array of the string to be changed
+     */
+    const handleUpdateCondition = async (e, index) => {
+        localConditions[index] = e.target.value;
+        setLocalConditions([...localConditions]);
+        await updateBackendData(localConditions);
+    };
+
+    const handleToggleEdit = () => {
+        setEditingConditions(!editingConditions);
+    };
+
+    const updateBackendData = async (conditionList) => {
+        // PUT the updated condition
+        const opt = {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                ...data,
+                conditions: conditionList,
+            }),
+        };
+
+        await fetch(GetPatientEndpoint(), opt)
+            .then((res) => {
+                if (res.status === 200) {
+                    refetch();
+                    setConditions(conditionList);
+                }
+            })
+            .catch((error) => {
+                console.error("Error during PUT:", error);
+            });
+    };
+
     return (
         <div className={styles.listNode}>
             <button className={styles.deleteButton} onClick={handleDelete}>
@@ -33,12 +92,50 @@ const ListNode = ({ refetch, data }) => {
             <h3 className={styles.name}>{data.name}</h3>
             <p className={styles.age}>Age: {data.age}</p>
             <div className={styles.conditions}>
-                <p>Conditions: </p>
-                <ul>
-                    {data.conditions.map((cond, index) => (
-                        <li key={index}>{cond}</li>
-                    ))}
-                </ul>
+                <p>Conditions:</p>
+                {editingConditions ? (
+                    <div>
+                        {localConditions.map((cond, index) => (
+                            <div>
+                                <input
+                                    onChange={(e) =>
+                                        handleUpdateCondition(e, index)
+                                    }
+                                    value={localConditions[index]}
+                                    key={index}
+                                ></input>
+                                <button
+                                    className={styles.removeButton}
+                                    onClick={() => handleRemoveCondition(index)}
+                                >
+                                    X
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <ul>
+                        {localConditions.map((cond, index) => (
+                            <li key={index}>{cond}</li>
+                        ))}
+                    </ul>
+                )}
+
+                {editingConditions && (
+                    <div>
+                        <input
+                            type="text"
+                            value={newCondition}
+                            onChange={(e) => setNewCondition(e.target.value)}
+                            placeholder="New condition"
+                        />
+                        <button onClick={handleAddCondition}>Add</button>
+                    </div>
+                )}
+
+                <button onClick={handleToggleEdit}>
+                    {editingConditions ? "Stop editing" : "Edit Conditions"}
+                </button>
             </div>
         </div>
     );
