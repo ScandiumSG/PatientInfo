81688285f075bb4dbaebda5bcf8079336ce87a25
Stian Gaustad <stian.k.gaustad@gmail.com>
2024-12-08 20:04:21 +0100

    Backend - Updated Patient documentation

--- Backend/Models/Patient.cs
+++ Backend/Models/Patient.cs
@@ -10,6 +10,10 @@
 
         public List<string> Conditions { get; set; } = new List<string>();
 
+        /// <summary>
+        /// Calculate the age of the person based on current time and the recorded DateOfBirth value.
+        /// </summary>
+        /// <returns>Int of the calculate age</returns>
         public int CalculateAge() 
         {
             DateTime currentTime = DateTime.Now;
